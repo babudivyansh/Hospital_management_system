b30379036da1853b2597ab2258dfc1308e86298a
babudivyansh <divyansh.verma525@gmail.com>
2024-03-09 11:46:24 +0530

    [Divyansh Babu]Add: Adding the patient route in this module.

--- main.py
+++ main.py
@@ -5,19 +5,21 @@
 
 @Last Modified by: Divyansh Babu
 
-@Last Modified time: 2024-03-08 14:37
+@Last Modified time: 2024-03-09 11:46
 
 @Title : Hospital Management app using Sanic and Mongodb.
 """
 from sanic import Sanic
 from Routes.doctors import app as dr
 from Routes.departments import app as di
+from Routes.patient import app as pr
 
 
 app = Sanic(__name__)
 
 app.blueprint(dr)
 app.blueprint(di)
+app.blueprint(pr)
 
 
 if __name__ == "__main__":
